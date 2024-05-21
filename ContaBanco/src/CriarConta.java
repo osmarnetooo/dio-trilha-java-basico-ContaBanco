7eae2a73f6947e462ee7f29b9164e156647c8cd8
Osmar Neto <osmar.para.trabalho@gmail.com>
2024-05-21 00:24:12 -0300

    editando alguns detalhes

--- ContaBanco/src/CriarConta.java
+++ ContaBanco/src/CriarConta.java
@@ -10,18 +10,18 @@ public class CriarConta {
 
     public void novaConta(){
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-            System.out.println("Por favor, digite seu primeiro nome: ");
-            primeiro_Nome = scanner.next();
-            System.out.println("Por favor, digite seu sobrenome: ");
-            sobrenome = scanner.next();
-            System.out.println("Por favor, digite o número da sua conta: ");
-            conta = scanner.nextInt();
-            System.out.println("Por favor, digite o número de sua Agência: ");
-            agencia = scanner.next();
-            System.out.println("Para validar sua conta você deve fazer um deposito acima de R$10,00 reais.");
-            System.out.println("Esse valor ficará disponível na CC para interações (saques, compras, investimentos, e etc).");
-            System.out.println("Informe o valor que você deseja deposiar: ");
-            saldo = scanner.nextDouble();
-    }
-    
+        System.out.println("Por favor, digite seu primeiro nome: ");
+        primeiro_Nome = scanner.next();
+        System.out.println("Por favor, digite seu sobrenome: ");
+        sobrenome = scanner.next();
+        System.out.println("Por favor, digite o número da sua conta: ");
+        conta = scanner.nextInt();
+        System.out.println("Por favor, digite o número de sua Agência: ");
+        agencia = scanner.next();
+        System.out.println("Para validar sua conta você deve fazer um deposito de qualquer quantia.");
+        System.out.println("Esse valor ficará disponível na CC para interações (saques, compras, investimentos, e etc).");
+        System.out.println("Informe o valor que você deseja deposiar: ");
+        saldo = scanner.nextDouble();
+    } 
+
 }
